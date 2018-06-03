dccff3fca633e5df1f770cad2b29ee8073b40a10
Jonathan Thom <jonathan.thom1990@gmail.com>
2018-06-03 12:32:47 -0700

    Saves top links to struct snapshot

--- crawler.go
+++ crawler.go
@@ -3,17 +3,28 @@ package main
 import (
 	"fmt"
 	"github.com/gocolly/colly"
+	"time"
 )
+
+type link struct {
+	URL string
+}
+
+type topLinksSnapshot struct {
+	Links   []link
+	FoundAt time.Time
+}
 
 func main() {
 	c := colly.NewCollector()
-	links := []string{}
+	snapshot := topLinksSnapshot{FoundAt: time.Now()}
 
 	c.OnHTML(".storylink", func(e *colly.HTMLElement) {
-		link := e.Attr("href")
-		links = append(links, link)
+		link := link{}
+		link.URL = e.Attr("href")
+		snapshot.Links = append(snapshot.Links, link)
 	})
 
 	c.Visit("https://news.ycombinator.com")
-	fmt.Println(links)
+	fmt.Println(snapshot)
 }
